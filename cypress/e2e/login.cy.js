11efebf8dc700d4d672e4b25375e3be64dde5376
alejandroere2023 <146302202+alejandroere2023@users.noreply.github.com>
2026-03-30 13:22:15 -0300

    commit1
    30/03/2026

---
+++ cypress/e2e/login.cy.js
@@ -0,0 +1,35 @@
+describe('Login - Flujo Principal', ()=>{
+    it('El Usuario inicia sesión correctamente', ()=>{
+        cy.viewport(1920, 1080)
+        cy.visit('https://testgrandt-resp.agea.com.ar/');
+
+        cy.get('#btnLogin').click()
+
+        //cy.get('input[type="email"]').type('test77@yopmail.com')
+        cy.origin('https://staging-micuenta.clarin.com', () => {
+
+            cy.get('#username', {
+                timeout: 15000
+            })
+            .click({ force: true })
+            .type('test77@yopmail.com', { force: true })
+
+            cy.get('button[data-action-button-primary="true"]').click()
+
+
+        })
+
+        cy.origin('https://staging-micuenta.clarin.com', () => {
+
+            cy.url().should('include', '/u/login/password')
+
+            cy.get('#password')
+                .should('exist')
+                .type('Testing1', { log: false })
+
+            cy.contains('button', 'Continuar').click()
+        })
+
+
+    })
+})
---
+++ cypress/e2e/transferenciaF11.cy.js
@@ -0,0 +1,191 @@
+describe('Transferencia F11', ()=>{
+
+//------------------------------Transferir Arquero------------------------------    
+    it('Transferencia Arquero', ()=>{
+        cy.viewport(1920, 1080)
+        cy.visit('https://testgrandt-resp.agea.com.ar/');
+
+        cy.get('#btnLogin').click()
+
+        //cy.get('input[type="email"]').type('test77@yopmail.com')
+        cy.origin('https://staging-micuenta.clarin.com', () => {
+
+            cy.get('#username', {timeout: 15000})
+            .click({ force: true })
+            .type('test80@yopmail.com', { force: true })
+
+            cy.get('button[data-action-button-primary="true"]').click()
+
+
+        })
+
+        cy.origin('https://staging-micuenta.clarin.com', () => {
+
+            cy.url().should('include', '/u/login/password')
+
+            cy.get('#password')
+                .should('exist')
+                .type('Testing1', { log: false })
+
+            cy.contains('button', 'Continuar').click()
+        })
+
+                // Espera a que aparezca el popup
+        cy.contains('Recibí nuestras notificaciones', { timeout: 15000 })
+        .should('be.visible')
+
+        // Cierra el popup
+        cy.contains('NO, GRACIAS')
+        .should('be.visible')
+        .click()
+    
+        //Cerrar Mascara
+
+        cy.get('[href="/mi-equipo/futbol-11"]').click();
+        cy.get('.bg-gdt-blue-header > .flex > span').click();
+        cy.get('.bg-gdt-blue-header > .flex > span').click();
+        //cy.get('.bg-gdt-blue-header > .flex > span').click();
+
+        //Selecciona Arquero
+        cy.get('#refPlayerField-1 > .h-15',  {timeout: 15000}).click();
+        cy.wait(2000)
+        //Selecciona Transferir en el Pop
+        cy.get('.max-h-11 > :nth-child(3)',  {timeout: 15000}).click();
+        cy.wait(2000)
+
+        //Selecciona Jugador
+        cy.get('[style="position: absolute; top: 91px; left: 0px; width: 100%; height: 91px;"] > .justify-between',  {timeout: 15000}).click();
+        cy.wait(2000)
+        //Confirma
+        cy.get('.mb-6 > .mt-8 > .relative',  {timeout: 15000}).click();
+        cy.wait(3000)
+    })
+
+//------------------------------Transferir Defensor------------------------------
+
+        it('Transferencia Defensor', ()=>{
+        cy.viewport(1920, 1080)
+        cy.visit('https://testgrandt-resp.agea.com.ar/');
+
+        cy.get('#btnLogin').click()
+
+        //cy.get('input[type="email"]').type('test77@yopmail.com')
+        cy.origin('https://staging-micuenta.clarin.com', () => {
+
+            cy.get('#username', {timeout: 15000})
+            .click({ force: true })
+            .type('test80@yopmail.com', { force: true })
+
+            cy.get('button[data-action-button-primary="true"]').click()
+
+
+        })
+
+        cy.origin('https://staging-micuenta.clarin.com', () => {
+
+            cy.url().should('include', '/u/login/password')
+
+            cy.get('#password')
+                .should('exist')
+                .type('Testing1', { log: false })
+
+            cy.contains('button', 'Continuar').click()
+        })
+
+                // Espera a que aparezca el popup
+        cy.contains('Recibí nuestras notificaciones', { timeout: 15000 })
+        .should('be.visible')
+
+        // Cierra el popup
+        cy.contains('NO, GRACIAS')
+        .should('be.visible')
+        .click()
+    
+        //Cerrar Mascara
+
+        cy.get('[href="/mi-equipo/futbol-11"]').click();
+        cy.get('.bg-gdt-blue-header > .flex > span').click();
+        cy.get('.bg-gdt-blue-header > .flex > span').click();
+        //cy.get('.bg-gdt-blue-header > .flex > span').click();
+
+        //Selecciona Arquero
+        cy.get('#refPlayerField-3 > .h-15',  {timeout: 15000}).click();
+        cy.wait(2000)
+        //Selecciona Transferir en el Pop
+        cy.get('.max-h-11 > :nth-child(3)',  {timeout: 15000}).click();
+        cy.wait(2000)
+
+        //Selecciona Jugador
+        cy.get('[style="position: absolute; top: 91px; left: 0px; width: 100%; height: 91px;"] > .justify-between',  {timeout: 15000}).click();
+        cy.wait(2000)
+        //Confirma
+        cy.get('.mb-6 > .mt-8 > .relative',  {timeout: 15000}).click();
+        cy.wait(3000)
+    })
+
+
+//------------------------------Transferir Volante------------------------------
+
+        it('Transferencia Volamte', ()=>{
+        cy.viewport(1920, 1080)
+        cy.visit('https://testgrandt-resp.agea.com.ar/');
+
+        cy.get('#btnLogin').click()
+
+        //cy.get('input[type="email"]').type('test77@yopmail.com')
+        cy.origin('https://staging-micuenta.clarin.com', () => {
+
+            cy.get('#username', {timeout: 15000})
+            .click({ force: true })
+            .type('test80@yopmail.com', { force: true })
+
+            cy.get('button[data-action-button-primary="true"]').click()
+
+
+        })
+
+        cy.origin('https://staging-micuenta.clarin.com', () => {
+
+            cy.url().should('include', '/u/login/password')
+
+            cy.get('#password')
+                .should('exist')
+                .type('Testing1', { log: false })
+
+            cy.contains('button', 'Continuar').click()
+        })
+
+                // Espera a que aparezca el popup
+        cy.contains('Recibí nuestras notificaciones', { timeout: 15000 })
+        .should('be.visible')
+
+        // Cierra el popup
+        cy.contains('NO, GRACIAS')
+        .should('be.visible')
+        .click()
+    
+        //Cerrar Mascara
+
+        cy.get('[href="/mi-equipo/futbol-11"]').click();
+        cy.get('.bg-gdt-blue-header > .flex > span').click();
+        cy.get('.bg-gdt-blue-header > .flex > span').click();
+        //cy.get('.bg-gdt-blue-header > .flex > span').click();
+
+        //Selecciona Arquero
+        cy.get('#refPlayerField-7 > .h-15',  {timeout: 15000}).click();
+        cy.wait(2000)
+        //Selecciona Transferir en el Pop
+        cy.get('.max-h-11 > :nth-child(3)',  {timeout: 15000}).click();
+        cy.wait(2000)
+
+        //Selecciona Jugador
+        cy.get('[style="position: absolute; top: 91px; left: 0px; width: 100%; height: 91px;"] > .justify-between',  {timeout: 15000}).click();
+        cy.wait(2000)
+        //Confirma
+        cy.get('.mb-6 > .mt-8 > .relative',  {timeout: 15000}).click();
+        cy.wait(3000)
+    })    
+
+
+    
+})
